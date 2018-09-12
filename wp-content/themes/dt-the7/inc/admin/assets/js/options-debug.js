9502ffb081999088404180febf2096ed7ee9d611
Juan Carlos Orrego <jcorrego@gmail.com>
2018-09-11 19:19:51 -0500

    demo folder

--- wp-content/themes/dt-the7/inc/admin/assets/js/options-debug.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-
-	$('.show-debug-info.button').on('click', function() {
-		$('#optionsframework-wrap').toggleClass('of-debug-show');
-	}).detach().insertBefore('.nav-tab-wrapper').show();
-
-});
